c2c1b3173d93ae2693ae47603e6ead0ee96ea048
DriftDron <58187194+DriftDron@users.noreply.github.com>
2019-12-10 05:07:22 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,10 +1,16 @@
-for(i = 0; i < 50; i++){
+for(i = 0; i < 10; i++){
 	db.students1.insertMany([
 		{"name": "John", "number": 123, "score": Math.random()*10 },
 		{"name": "Vika", "number": 123, "score": Math.random()*10 },
-		{"name": "Ivan", "number": 123, "score": Math.random()*10 }])}
+		{"name": "Ivan", "test": 123, "score": Math.random()*10 }])}
 
-var check = db.students1.findOne()
-for (var key in check){
-	print(key, typeof check[key]);
+var myCursor = db.students1.find()
+while (myCursor.hasNext()){
+	obj = myCursor.next();
+	for (var key in obj){
+		print(key, typeof obj[key]);
+	}
 }
+db.students1.drop()
+
+	
